685d281f627f97aa80bfd8e74cbd9acd8385f13c
gastoncaute <gastoncaute@gmail.com>
2023-10-04 17:47:00 -0300

    first commit

---
+++ src/app/Components/HeaderButtons/Categories.tsx
@@ -0,0 +1,10 @@
+export default function Categories() {
+    return (
+        <section>
+            <button>Clima</button>
+            <button>Deporte</button>
+            <button>Municipal</button>
+            <button>Localidad</button>
+        </section>
+    )
+}
