f8ebd721b382595400230406a4b446e181368b1a
gastoncaute <gastoncaute@gmail.com>
2023-10-16 20:06:56 -0300

    se  agrega tweet

--- src/app/Components/SectionComponents/Noticia.tsx
+++ src/app/Components/SectionComponents/Noticia.tsx
@@ -11,84 +11,83 @@ export default function Noticia() {
                         {categoria}
                     </p>
                     <div className="grid grid-cols-2 grid-rows-4">
-                    <div className="col-span-2 row-span-2 p-4 flex items-center">
-                        <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-8">
-                            <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor">
-                                <Image 
-                                src={"/images/img1.jpg"}
-                                alt="Mi imagen"
-                                width={500}
-                                height={265}
-                                />
-                            </div>
-                            <div className="col-start-2 col-end-3 flex flex-col items-center justify-center m-4">
-                                <p className="text-5xl mb-8 font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá dura derrota ante Oberá</p>
-                                <p className="text-3xl">El “Milrayitas” cayó como visitante por 105 a 71 en su debut en la Liga Nacional. Volverá a jugar el miércoles en Corrientes.</p>
-                            </div>
-                        </Link>
+                        <div className="col-span-2 row-span-2 p-4 flex items-center">
+                            <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-8">
+                                <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor">
+                                    <Image 
+                                    src={"/images/img1.jpg"}
+                                    alt="Mi imagen"
+                                    width={500}
+                                    height={265}
+                                    />
+                                </div>
+                                <div className="col-start-2 col-end-3 flex flex-col items-center justify-center m-4">
+                                    <p className="text-5xl mb-8 font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá dura derrota ante Oberá</p>
+                                    <p className="text-3xl">El “Milrayitas” cayó como visitante por 105 a 71 en su debut en la Liga Nacional. Volverá a jugar el miércoles en Corrientes.</p>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="col-start-2 col-end-3 row-start-3 row-end-4 p-4 flex justify-center items-center">
+                            <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-4">
+                                <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor p-4">
+                                    <Image 
+                                        src={"/images/img1.jpg"}
+                                        alt="Mi imagen"
+                                        width={250}
+                                        height={150}
+                                    />
+                                </div>
+                                <div className="col-start-2 col-end-3 flex items-center p-4">
+                                    <p className="text-2xl font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá </p>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="col-start-1 col-end-2 row-start-4 row-end-5 p-4 flex justify-center items-center">
+                            <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-4">
+                                <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor p-4">
+                                    <Image 
+                                        src={"/images/img1.jpg"}
+                                        alt="Mi imagen"
+                                        width={250}
+                                        height={150}
+                                    />
+                                </div>
+                                <div className="col-start-2 col-end-3 flex items-center p-4">
+                                    <p className="text-2xl font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá</p>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="col-start-2 col-end-3 row-start-4 row-end-5 p-4 flex justify-center items-center">
+                            <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-4">
+                                <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor p-4">
+                                    <Image 
+                                        src={"/images/img1.jpg"}
+                                        alt="Mi imagen"
+                                        width={250}
+                                        height={150}
+                                    />
+                                </div>
+                                <div className="col-start-2 col-end-3 flex items-center p-4">
+                                    <p className="text-2xl font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá</p>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="col-start-1 col-end-2 row-start-3 row-end-4 p-4 flex justify-center items-center">
+                            <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-4">
+                                <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor p-4">
+                                    <Image 
+                                        src={"/images/img1.jpg"}
+                                        alt="Mi imagen"
+                                        width={250}
+                                        height={150}
+                                    />
+                                </div>
+                                <div className="col-start-2 col-end-3 flex items-center p-4">
+                                    <p className="text-2xl font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá</p>
+                                </div>
+                            </Link>
+                        </div>
                     </div>
-                    <div className="col-start-2 col-end-3 row-start-3 row-end-4 p-4 flex justify-center items-center">
-                <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-4">
-                    <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor p-4">
-                        <Image 
-                            src={"/images/img1.jpg"}
-                            alt="Mi imagen"
-                            width={250}
-                            height={150}
-                        />
-                    </div>
-                    <div className="col-start-2 col-end-3 flex items-center p-4">
-                        <p className="text-2xl font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá </p>
-                    </div>
-                </Link>
-            </div>
-            <div className="col-start-1 col-end-2 row-start-4 row-end-5 p-4 flex justify-center items-center">
-                <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-4">
-                    <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor p-4">
-                        <Image 
-                            src={"/images/img1.jpg"}
-                            alt="Mi imagen"
-                            width={250}
-                            height={150}
-                        />
-                    </div>
-                    <div className="col-start-2 col-end-3 flex items-center p-4">
-                        <p className="text-2xl font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá</p>
-                    </div>
-                </Link>
-            </div>
-            <div className="col-start-2 col-end-3 row-start-4 row-end-5 p-4 flex justify-center items-center">
-                <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-4">
-                    <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor p-4">
-                        <Image 
-                            src={"/images/img1.jpg"}
-                            alt="Mi imagen"
-                            width={250}
-                            height={150}
-                        />
-                    </div>
-                    <div className="col-start-2 col-end-3 flex items-center p-4">
-                        <p className="text-2xl font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá</p>
-                    </div>
-                </Link>
-            </div>
-            <div className="col-start-1 col-end-2 row-start-3 row-end-4 p-4 flex justify-center items-center">
-                <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-4">
-                    <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor p-4">
-                        <Image 
-                            src={"/images/img1.jpg"}
-                            alt="Mi imagen"
-                            width={250}
-                            height={150}
-                        />
-                    </div>
-                    <div className="col-start-2 col-end-3 flex items-center p-4">
-                        <p className="text-2xl font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá</p>
-                    </div>
-                </Link>
-            </div>
-                    </div>
-            
                 </div >
             ))}
         </>
--- src/app/noticepage/page.tsx
+++ src/app/noticepage/page.tsx
@@ -1,10 +1,11 @@
 import Header from "@/app/Components/Header";
 import Footer from "../Components/Footer";
 import Image from "next/image";
+import { Tweet } from "react-tweet";
 
 export default function Page() {
     return(
-        <section className="h-screen flex flex-col justify-between">
+        <>
             <Header />
             <main className="grid grid-cols-3 mx-48 my-24 text-black">
                 <div className="flex items-center mt-12 mb-4">
@@ -32,11 +33,14 @@ export default function Page() {
                         <p className="text-2xl">Un colectivo perdió el control y chocó contra un árbol durante la mañana de este martes en el barrio Villa Primera de Mar del Plata. Doce personas debieron ser asistidas por un ambulancias del Sistema de Atención Médica de Emergencias (SAME).
                         <br />
                         {/* @ts-ignore */}
-                        <blockquote className="twitter-tweet"><p lang="fr" dir="ltr">Messi es arte.<a href="https://t.co/O6oyErjaLI">pic.twitter.com/O6oyErjaLI</a></p>&mdash; Messismo (@Messismo10) <a href="https://twitter.com/Messismo10/status/1712994909173510367?ref_src=twsrc%5Etfw">October 14, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                        <blockquote class="twitter-tweet"><p lang="fr" dir="ltr">Messi es arte.<a href="https://t.co/O6oyErjaLI">pic.twitter.com/O6oyErjaLI</a></p>&mdash; Messismo (@Messismo10) <a href="https://twitter.com/Messismo10/status/1712994909173510367?ref_src=twsrc%5Etfw">October 14, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                         Según confirmaron fuentes oficiales a Qué digital, el siniestro vial se produjo durante la mañana de este martes en la avenida Libertad y Chaco donde un colectivo de la línea 531 que conducía un joven de 29 años perdió el control y chocó contra un árbol.
                         {/* @ts-ignore */}
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/4YizH2DQNxs?si=4aPggxN9A0JLuvmo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                         </p>
+                        <div className="-z-10">
+                            <Tweet id="1712994909173510367" />
+                        </div>
                     </div>
                     <div className="row-start-4 row-end-5 p-8 flex justify-center items-center">
                         <p className="border border-black w-full h-full">Comentaios</p>
@@ -50,6 +54,6 @@ export default function Page() {
                 </div>
             </main>
             <Footer />
-        </section>
+        </>
     )
 }
