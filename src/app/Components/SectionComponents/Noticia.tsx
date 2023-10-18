df4531a685ea64a4f9d7766cd4deaa9edb33b11e
gastoncaute <gastoncaute@gmail.com>
2023-10-18 18:07:36 -0300

    noticias

--- src/app/Components/SectionComponents/Noticia.tsx
+++ src/app/Components/SectionComponents/Noticia.tsx
@@ -21,8 +21,8 @@ export default async function Noticia() {
     const noticias = await obtenerNoticias()
     const noticiasPorCategoria: { [key: string]: Noticias[] } = {};
 
-    const categorias = ["Mas Recientes", "Policiales", 'Politica', 'Economia', 'Sociedad', 'Deporte', 'Cultura', 'Tendencias', 'Internacional', 'Fotos']
-
+    const titulo = "Peñarol debutó en la nueva temporada con una dura derrota ante Oberá"
+    const bajada = 'El “Milrayitas” cayó como visitante por 105 a 71 en su debut en la Liga Nacional. Volverá a jugar el miércoles en Corrientes.'
 
     noticias.forEach((noticia: Noticias) => {
         if (!noticiasPorCategoria[noticia.category]) {
@@ -32,129 +32,51 @@ export default async function Noticia() {
     });
     for (const categoria in noticiasPorCategoria) {
         noticiasPorCategoria[categoria].sort((a, b) => b.price - a.price)
-    }
-    return(
+    } return (
         <>
             {Object.keys(noticiasPorCategoria).map((categoria) => (
                 <div key={categoria} className="col-start-2 col-end-6 rounded-3xl mt-24 m-8 border border-pageColor">
-                    <p className="m-4 pb-2 flex justify-center text-2xl border-b border-pageColor">
+                    <Link href={''} className="m-4 pb-2 flex justify-center text-2xl border-b border-pageColor">
                         {categoria}
-                    </p>
-                    {noticiasPorCategoria[categoria].map((noticia) => (
-                        <div key={noticia.id} className="col-start-1 col-end-3 p-4 flex items-center">
-                            <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-8">
-                                <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor">
-                                    <img
-                                        src={noticia.image}
-                                        alt={noticia.title}
-                                        width={200}
-                                        height={100}
-                                    />
+                    </Link>
+                    <div className="grid grid-cols-2 grid-rows-4">
+                        {noticiasPorCategoria[categoria].map((noticia, index) => {
+                            if (index >= 5) {
+                                return null
+                            } return (
+                                <div
+                                key={noticia.id}
+                                className={
+                                    index === 0 ? "col-start-1 col-end-3 row-start-1 row-end-3 p-4" :
+                                    index === 1 ? "col-start-1 col-end-2 row-start-3 row-end-4 p-4" :
+                                    index === 2 ? "col-start-2 col-end-3 row-start-3 row-end-4 p-4" :
+                                    index === 3 ? "col-start-1 col-end-2 row-start-4 row-end-5 p-4" :
+                                    "col-start-2 col-end-3 row-start-4 row-end-5 p-4"
+                                }>
+                                    <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-8">
+                                        <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor">
+                                            <img
+                                                src={'/images/img1.jpg'}
+                                                alt={titulo}
+                                                width={index === 0 ? 500 : 220}
+                                                height={index === 0 ? 250 : 130}
+                                            />
+                                        </div>
+                                        <div className="col-start-2 col-end-3 flex flex-col items-center justify-center m-4">
+                                            <p className={index !== 0 ? "text-2xl font-bold" : "text-5xl mb-8 font-bold"}>
+                                                {noticia.title}
+                                            </p>
+                                            {index === 0 && <p className="text-3xl">
+                                                {bajada}
+                                            </p>}
+                                        </div>
+                                    </Link>
                                 </div>
-                                <div className="col-start-2 col-end-3 flex flex-col items-center justify-center m-4">
-                                    <p className="text-5xl mb-8 font-bold">{noticia.title}</p>
-                                    <p className="text-3xl">{noticia.description}</p>
-                                </div>
-                            </Link>
-                        </div>
-                    ))}
+                            )
+                        })}
+                    </div>
                 </div>
             ))}
         </>
-
-
-        // <>
-        //     {categorias.map((categoria, index) => (
-        //         <div key={index} className="col-start-2 col-end-6 rounded-3xl mt-24 m-8 border border-pageColor">
-        //             <div className="m-4 pb-2 flex justify-center text-2xl border-b border-pageColor">
-        //                 <Link href={"/"}>
-        //                     {categoria}
-        //                 </Link>
-        //             </div>
-        //             <div className="grid grid-cols-2 grid-rows-4">
-        //                 <div className="col-span-2 row-span-2 p-4 flex items-center">
-        //                     <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-8">
-        //                         <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor">
-        //                             <Image
-        //                             src={"/images/img1.jpg"}
-        //                             alt="Mi imagen"
-        //                             width={500}
-        //                             height={250}
-        //                             />
-        //                         </div>
-        //                         <div className="col-start-2 col-end-3 flex flex-col items-center justify-center m-4">
-        //                             <p className="text-5xl mb-8 font-bold">
-        //                                 Peñarol debutó en la nueva temporada con una dura derrota ante Oberá
-        //                             </p>
-        //                             <p className="text-3xl">
-        //                                 El “Milrayitas” cayó como visitante por 105 a 71 en su debut en la Liga Nacional. Volverá a jugar el miércoles en Corrientes.
-        //                             </p>
-        //                         </div>
-        //                     </Link>
-        //                 </div>
-        //                 <div className="col-start-2 col-end-3 row-start-3 row-end-4 p-4 flex justify-center items-center">
-        //                     <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-4">
-        //                         <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor p-4">
-        //                             <Image 
-        //                                 src={"/images/img1.jpg"}
-        //                                 alt="Mi imagen"
-        //                                 width={250}
-        //                                 height={150}
-        //                             />
-        //                         </div>
-        //                         <div className="col-start-2 col-end-3 flex items-center p-4">
-        //                             <p className="text-2xl font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá</p>
-        //                         </div>
-        //                     </Link>
-        //                 </div>
-        //                 <div className="col-start-1 col-end-2 row-start-4 row-end-5 p-4 flex justify-center items-center">
-        //                     <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-4">
-        //                         <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor p-4">
-        //                             <Image 
-        //                                 src={"/images/img1.jpg"}
-        //                                 alt="Mi imagen"
-        //                                 width={250}
-        //                                 height={150}
-        //                             />
-        //                         </div>
-        //                         <div className="col-start-2 col-end-3 flex items-center p-4">
-        //                             <p className="text-2xl font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá</p>
-        //                         </div>
-        //                     </Link>
-        //                 </div>
-        //                 <div className="col-start-2 col-end-3 row-start-4 row-end-5 p-4 flex justify-center items-center">
-        //                     <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-4">
-        //                         <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor p-4">
-        //                             <Image 
-        //                                 src={"/images/img1.jpg"}
-        //                                 alt="Mi imagen"
-        //                                 width={250}
-        //                                 height={150}
-        //                             />
-        //                         </div>
-        //                         <div className="col-start-2 col-end-3 flex items-center p-4">
-        //                             <p className="text-2xl font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá</p>
-        //                         </div>
-        //                     </Link>
-        //                 </div>
-        //                 <div className="col-start-1 col-end-2 row-start-3 row-end-4 p-4 flex justify-center items-center">
-        //                     <Link href={"/noticepage"} className="w-full h-full grid grid-cols-2 border rounded-3xl border-pageColor py-4">
-        //                         <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor p-4">
-        //                             <Image 
-        //                                 src={"/images/img1.jpg"}
-        //                                 alt="Mi imagen"
-        //                                 width={250}
-        //                                 height={150}
-        //                             />
-        //                         </div>
-        //                         <div className="col-start-2 col-end-3 flex items-center p-4">
-        //                             <p className="text-2xl font-bold">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá</p>
-        //                         </div>
-        //                     </Link>
-        //                 </div>
-        //             </div>
-        //         </div >
-        //     ))}
-        // </>
     )
 }
