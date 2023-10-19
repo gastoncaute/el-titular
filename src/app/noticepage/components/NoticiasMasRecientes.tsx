2d05d0ba0ca25f6dcf3552c5d69cac25a6b7c2be
gastoncaute <gastoncaute@gmail.com>
2023-10-19 12:08:18 -0300

    se agrega noticias mas recientes a pagenotice

--- src/app/noticepage/components/NoticiasMasRecientes.tsx
+++ src/app/noticepage/components/NoticiasMasRecientes.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
+"use client"
+import Link from 'next/link'
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export async function obtenerNoticias() {
     const res = await fetch("https://fakestoreapi.com/products")
@@ -16,24 +19,40 @@ type Noticias = {
     price: number;
 }
 
-export default async function NoticiasMasRecientes() {
-    const noticias = await obtenerNoticias()
+export default function NoticiasMasRecientes() {
+    const titulo = "Peñarol debutó en la nueva temporada con una dura derrota ante Oberá"
+    const bajada = 'El “Milrayitas” cayó como visitante por 105 a 71 en su debut en la Liga Nacional. Volverá a jugar el miércoles en Corrientes.'
 
-  return (
-    <div>
-        {noticias.map((noticia: Noticias) => (
-            <div key={noticia.id} className='p-4 flex item-center'>
-                <img
-                src={noticia.image}
-                alt=""
-                width={100}
-                height={50}
-                />
-                {noticia.title}
-                <br />
-                {noticia.price}
-            </div>
-        ))}
-    </div>
-  )
+    const [noticias, setNoticias] = useState<Noticias[]>([])
+
+    useEffect(() => {
+        const fetchNoticas = async () => {
+            const noticiasData = await obtenerNoticias()
+            noticiasData.sort((a: any, b: any) => b.price - a.price)
+            const primeras5Noticas = noticiasData.slice(0 , 5)
+            setNoticias(primeras5Noticas)
+        }
+        fetchNoticas()
+    }, [])
+
+
+    return (
+        <div className='flex flex-col item-center p-4'>
+            <h1 className='flex justify-center mb-4 text-2xl font-bold'>Más Recientes</h1>
+            {noticias.map((noticia: Noticias) => (
+                <Link href={'/'} key={noticia.id} className='flex p-4 m-2 border border-black rounded-3xl'>
+                    <img
+                    className='mx-4'
+                    src={'/images/img1.jpg'}
+                    alt=""
+                    width={'40%'}
+                    height={'40%'}
+                    />
+                    <p className='text-lg font-bold pl-4 border-l border-black'>
+                        {titulo}
+                    </p>
+                </Link>
+            ))}
+        </div>
+    )
 }
--- src/app/noticepage/page.tsx
+++ src/app/noticepage/page.tsx
@@ -9,18 +9,18 @@ export default function Page() {
         <>
             <Header />
             <main className="grid grid-cols-3 mx-48 my-24 text-black">
-                <div className="flex items-center mt-12 mb-4">
+                <div className="row-span-1 flex items-center mt-12 mb-4">
                     <p className="border border-black rounded-3xl p-4">
                         Tipo de categoria
                     </p>
                 </div>
-                <div className="col-start-1 col-end-3 row-start-2 row-end-3 border border-black rounded-3xl  p-4">
+                <div className="col-start-1 col-end-3 row-span-2 border border-black rounded-3xl  p-4">
                     <div className="row-start-1 row-end-2 mb-8">
                         <h1 className="text-5xl font-bold mb-4">Peñarol debutó en la nueva temporada con una dura derrota ante Oberá</h1>
                         <h3 className="text-2xl border-b border-black pb-4">El “Milrayitas” cayó como visitante por 105 a 71 en su debut en la Liga Nacional. Volverá a jugar el miércoles en Corrientes.</h3>
                         <h5 className="flex justify-end py-4 border-b border-black">Martes 10 de Octubre</h5>
                     </div>
-                    <div className="row-start-2 row-end-3">
+                    <div className="">
                         <Image 
                             src={"/images/img1.jpg"}
                             alt="Mi imagen"
@@ -29,7 +29,7 @@ export default function Page() {
                         />
                         <h5 className=" py-4 border-b border-black">Epigrafe</h5>
                     </div>
-                    <div className="row-start-3 row-end-4 border-b border-black pb-8">
+                    <div className="border-b border-black pb-8">
                         <p className="my-8">Autor</p>
                         <p className="text-2xl">Un colectivo perdió el control y chocó contra un árbol durante la mañana de este martes en el barrio Villa Primera de Mar del Plata. Doce personas debieron ser asistidas por un ambulancias del Sistema de Atención Médica de Emergencias (SAME).
                         <br />
@@ -41,11 +41,11 @@ export default function Page() {
                             <Tweet id="1712994909173510367" />
                         </div>
                     </div>
-                    <div className="row-start-4 row-end-5 p-8 flex justify-center items-center">
+                    <div className="p-8 flex justify-center items-center">
                         <p className="border border-black w-full h-full">Comentaios</p>
                     </div>
                 </div>
-                <div className="col-start-3 col-end-4 row-start-2 row-end-3 border border-black rounded-3xl ml-12 flex items-center justify-center h-min">
+                <div className="col-start-3 col-end-4 row-span-2 border border-black rounded-3xl ml-12 flex items-center justify-center h-min">
                     Noticias mas recientes del tipo de categoria
                 </div>
                 <div className="col-start-2 col-end-3 row-span-3 border border-black rounded-3xl mt-12 flex items-center justify-center">
