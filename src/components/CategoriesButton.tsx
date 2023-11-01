2d97a8a936ec8d6deb83b785d7319fa616c9d541
gastoncaute <gastoncaute@gmail.com>
2023-11-01 14:21:58 -0300

    Se agrega ruta dinamica a las categorias

--- src/components/CategoriesButton.tsx
+++ src/components/CategoriesButton.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
-export default function CategoriesButton() {
+export default  function CategoriesButton() {
     const categorias = ["Policiales", 'Politica', 'Economia', 'Sociedad', 'Deporte', 'Cultura', 'Tendencias', 'Internacional', 'Infografias']
     return (
         <>
-            {categorias.map((categoria, index) => (
-                <Link key={index} href={``}>
+            {categorias.map((categoria: string) => (
+                <Link key={categoria} href={`/pages/categorypage/${categoria}`}>
                     <p className="flex items-center p-3 border-x h-2">
                     {categoria}
                     </p>
