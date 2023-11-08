433690e39707adaa52b8313394f7fb6dd1fd62b2
gastoncaute <gastoncaute@gmail.com>
2023-11-08 12:02:22 -0300

    se modifica el titulo

--- src/components/HeaderComponents/Components/Title.tsx
+++ src/components/HeaderComponents/Components/Title.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useState } from "react";
 import CategoriesButton from "../../CategoriesButton";
-import Image from "next/image";
 import Link from "next/link";
+import "@/app/globals.css";
 
 export default function Title() {
   const [scrollY, setScrollY] = useState(0);
@@ -25,25 +25,22 @@ export default function Title() {
             ${scrollY ? "-translate-y-24" : "translate-y-0"}
             `}
     >
-      <div className="grid grid-cols-3 items-center bg-gradient-to-b from-pageColor to-gradientColor rounded-b-3xl border-l border-r border-b border-white w-65rem h-24">
-        <Image
-          className="ml-16"
-          src="/android-chrome-512x512.png"
-          alt="Icono"
-          width="64"
-          height="64"
-        />
-        <Link href={"/"} className="font-bold text-7xl flex justify-center">
-          El Titular
+      <h1 className="flex justify-center items-center bg-gradient-to-b from-pageColor to-gradientColor rounded-b-3xl border-l border-r border-b border-white w-65rem h-24">
+        <Link
+          href={"/"}
+          className="font-bold text-7xl flex justify-center title"
+        >
+          <p className="text-white mr-4">EL</p>
+          <p className="text-backgraundColor">TITULAR</p>
         </Link>
-      </div>
-      <div className="flex items-center bg-gradient-to-b from-pageColor to-gradientColor rounded-b-3xl border-l border-r border-b border-white w-60rem h-12 px-4">
+      </h1>
+      <nav className="flex items-center bg-gradient-to-b from-pageColor to-gradientColor rounded-b-3xl border-l border-r border-b border-white w-60rem h-12 px-4">
         <ul className="w-full flex items-center justify-evenly">
           <li className="flex justify-evenly w-full">
             <CategoriesButton />
           </li>
         </ul>
-      </div>
+      </nav>
     </section>
   );
 }
