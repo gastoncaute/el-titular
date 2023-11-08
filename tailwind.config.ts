5ba2bbac15e5481791f278f9e2877e194d002dd3
gastoncaute <gastoncaute@gmail.com>
2023-11-08 11:30:17 -0300

    se agregan estilos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,43 +1,43 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        pageColor: '#86082E',
-        gradientColor: '#A32D41',
+        pageColor: "#86082E",
+        gradientColor: "#A32D41",
       },
       width: {
-        '65rem': '65rem',
-        '60rem' : '60rem'
+        "65rem": "65rem",
+        "60rem": "60rem",
       },
       maxWidth: {
-        '500px' : '500px',
-        '220px' : '220px'
+        "500px": "500px",
+        "220px": "220px",
       },
       maxHeight: {
-        '400px' : '400px',
-        '130px' : '130px',
-        '220px' : '220px'
+        "400px": "400px",
+        "130px": "130px",
+        "220px": "220px",
       },
       transitionDuration: {
-        '1500': '1500ms'
+        "1500": "1500ms",
       },
       translate: {
-        '200' : '200%'
-      }
+        "200": "200%",
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
