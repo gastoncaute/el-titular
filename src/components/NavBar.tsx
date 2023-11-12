e975eca9a8faec828590a12689b9dcebbdf4c95b
gastoncaute <gastoncaute@gmail.com>
2023-11-12 13:34:31 -0300

    se agregan redireccion a navabr

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,18 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
     <nav className="bg-gradient-to-b from-pageColor to-gradientColor w-96 h-screen flex flex-col absolute p-7 -ml-12 -mt-6">
       <ul className="flex flex-col h-full m-3 mt-8">
         <li className="py-4 text-xl headersButtons w-min">
-          <a href="/">Inicio</a>
+          <Link href="/">Inicio</Link>
         </li>
-        <li className="py-4 text-xl headersButtons">Ultimas Noticias</li>
-        <li className="py-4 text-xl headersButtons w-min">Infografias</li>
+        <li className="py-4 text-xl headersButtons">
+          <Link href={"/pages/masrecientes"}>Ultimas Noticias</Link>
+        </li>
+        <li className="py-4 text-xl headersButtons w-min">
+          <Link href={"/pages/infografiaspage/Infografias"}>Infografias</Link>
+        </li>
       </ul>
     </nav>
   );
