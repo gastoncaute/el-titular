cb32087c8f6803fd63d5478dc327f280cfcd84a6
gastoncaute <gastoncaute@gmail.com>
2023-11-15 12:42:47 -0300

    responsive 600px page ultimas noticias

--- src/app/pages/masrecientes/page.tsx
+++ src/app/pages/masrecientes/page.tsx
@@ -24,7 +24,7 @@ export default async function page() {
   return (
     <>
       <Header />
-      <section className="grid grid-cols-2 mx-48 my-24 text-black">
+      <section className="grid grid-cols-2 mx-48 my-24 text-black main_section_ultimasNoticias">
         <div className="row-span-1 flex items-center mt-12 mb-4">
           <Link
             href={"/pages/masrecientes"}
@@ -33,29 +33,33 @@ export default async function page() {
             Ultimas Noticias
           </Link>
         </div>
-        <article className="col-start-1 col-end-3 border border-pageColor rounded-3xl h-min p-4 flex flex-col items-center">
+        <article className="col-start-1 col-end-3 border border-pageColor rounded-3xl h-min p-4 flex flex-col items-center ultimasNoticias_article">
           {noticias.slice(0, 5).map((noticia: Noticia, index: number) => {
             const noticeImageUrl = noticeImageUrls[index];
             return (
               <Link
                 href={`/pages/noticepage/${noticia.title}`}
-                className="w-full h-min grid grid-cols-2 border rounded-3xl border-pageColor py-8 my-8"
+                className="w-full h-min grid grid-cols-2 border rounded-3xl border-pageColor py-8 my-8 ultimasNoticias_link"
                 key={index}
               >
-                <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor">
-                  <div>
-                    <Image
-                      className="max-h-300px max-w-min"
-                      src={noticeImageUrl}
-                      alt={noticia.title}
-                      height={250}
-                      width={400}
-                    />
-                  </div>
+                <div className="col-start-1 col-end-2 flex justify-center items-center border-r border-pageColor ultimasNoticias_image_content">
+                  <Image
+                    className="max-h-300px max-w-min ultimasNoticias_image"
+                    src={noticeImageUrl}
+                    alt={noticia.title}
+                    height={250}
+                    width={400}
+                  />
                 </div>
-                <div className="col-start-2 col-end-3 flex flex-col items-center justify-center m-4">
-                  <h1 className={"text-3xl mb-8 font-bold"}>{noticia.title}</h1>
-                  <h2 className="text-xl">{noticia.bajada}</h2>
+                <div className="col-start-2 col-end-3 flex flex-col items-center justify-center m-4 ultimasNoticias_title_component">
+                  <h1
+                    className={"text-3xl mb-8 font-bold ultimasNoticias_title"}
+                  >
+                    {noticia.title}
+                  </h1>
+                  <h2 className="text-xl ultimasNoticias_bajada">
+                    {noticia.bajada}
+                  </h2>
                 </div>
               </Link>
             );
