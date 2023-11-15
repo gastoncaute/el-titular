f3f3f71f965f22c50b97e4b8f967754592cde254
gastoncaute <gastoncaute@gmail.com>
2023-11-15 15:22:20 -0300

    se elimina tendencias

--- src/components/SectionComponents/Section.tsx
+++ src/components/SectionComponents/Section.tsx
@@ -1,5 +1,4 @@
 import Clima from "./components/Clima";
-import Tendencias from "./components/Tendencias";
 import Noticia from "./components/Noticia";
 
 export default function Section() {
@@ -7,7 +6,6 @@ export default function Section() {
     <section className="w-full h-full grid grid-cols-6 grid-rows-1 py-32 main_section">
       <Clima />
       <Noticia />
-      <Tendencias />
     </section>
   );
 }
