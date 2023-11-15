650ec5c9ca84b6783c0441e1bd72e7fd349fd7ff
gastoncaute <gastoncaute@gmail.com>
2023-11-14 23:35:22 -0300

    responsive main section 600px

--- src/components/SectionComponents/Section.tsx
+++ src/components/SectionComponents/Section.tsx
@@ -4,7 +4,7 @@ import Noticia from "./components/Noticia";
 
 export default function Section() {
   return (
-    <section className="w-full h-full grid grid-cols-6 grid-rows-1 py-32">
+    <section className="w-full h-full grid grid-cols-6 grid-rows-1 py-32 main_section">
       <Clima />
       <Noticia />
       <Tendencias />
--- src/components/SectionComponents/components/Tendencias.tsx
+++ src/components/SectionComponents/components/Tendencias.tsx
@@ -1,6 +1,6 @@
 export default function Tendencias() {
   return (
-    <div className="col-start-6 col-end-7 flex row-start-1 justify-center">
+    <div className="col-start-6 col-end-7 flex row-start-1 justify-center clima_widget">
       <div className="border border-pageColor m-8 fixed w-1/6 h-5/6 rounded-3xl flex justify-center items-center -mt-8 ml-4">
         Tendencias
       </div>
