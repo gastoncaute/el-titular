fcc6b3decdbbeb05f8f3c109cf684b45b6bc022c
gastoncaute <gastoncaute@gmail.com>
2023-11-21 19:33:54 -0300

    se arregla ruta de fotografias

--- src/components/Buttons/CategoriesButton.tsx
+++ src/components/Buttons/CategoriesButton.tsx
@@ -19,7 +19,7 @@ export default function CategoriesButton() {
           key={categoria}
           className="flex items-center p-3 border-x h-2 headersButtons category_nav_li"
           href={
-            categoria === "Infografias"
+            categoria === "Infografias" || "Fotografias"
               ? `/pages/fotospage/${categoria}`
               : `/pages/categorypage/${categoria}`
           }
