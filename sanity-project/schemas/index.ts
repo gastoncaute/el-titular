b9e9a21076d8b21efc2a10e4b24f9c1d70e0864e
gastoncaute <gastoncaute@gmail.com>
2023-11-21 20:08:32 -0300

    style form

--- sanity-project/schemas/index.ts
+++ sanity-project/schemas/index.ts
@@ -3,4 +3,4 @@ import fotos from './fotos'
 import noticias from './noticias'
 import videos from './videos'
 
-export const schemaTypes = [noticias, author, fotos, videos]
+export const schemaTypes = [author, noticias, fotos, videos]
