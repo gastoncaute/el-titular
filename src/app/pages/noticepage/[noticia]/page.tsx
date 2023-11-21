b0b2f4d091551cc2324f26e3aa07e90c6e950ad5
gastoncaute <gastoncaute@gmail.com>
2023-11-21 14:01:22 -0300

    opcion imagens png

--- src/app/pages/noticepage/[noticia]/page.tsx
+++ src/app/pages/noticepage/[noticia]/page.tsx
@@ -25,15 +25,18 @@ export default async function Page({ params }: any) {
     const modifiedRef = String(ref)
       .replace("image-", "")
       .replace("-jpg", ".jpg")
-      .replace("-webp", ".webp");
+      .replace("-webp", ".webp")
+      .replace("-png", ".png");
     const baseUrl = "https://cdn.sanity.io/images/lrwm6m86/production/";
     return baseUrl + modifiedRef;
   });
+
   const modifyImageUrl = (imageRef: any) => {
     const modifiedRef = String(imageRef)
       .replace("image-", "")
       .replace("-jpg", ".jpg")
-      .replace("-webp", ".webp");
+      .replace("-webp", ".webp")
+      .replace("-png", ".png");
     const baseUrl = "https://cdn.sanity.io/images/lrwm6m86/production/";
     return baseUrl + modifiedRef;
   };
