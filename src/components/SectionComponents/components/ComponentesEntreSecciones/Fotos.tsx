30d8e33e096040cbfc5700242830f09fa6eda95b
gastoncaute <gastoncaute@gmail.com>
2023-11-22 12:32:46 -0300

    section fotos en mainpage

--- src/components/SectionComponents/components/ComponentesEntreSecciones/Fotos.tsx
+++ src/components/SectionComponents/components/ComponentesEntreSecciones/Fotos.tsx
@@ -1,5 +1,41 @@
+import { Foto } from "@/types/componentes.types";
+import { modifyImageUrl } from "@/utils/modifyImageUrl";
+import { obtenerFotos } from "@/utils/obtenerFotos";
+import Image from "next/image";
 import React from "react";
 
-export default function Fotos({ categoria }: { categoria: any }) {
-  return <div>Fotos</div>;
+export default async function Fotos({ categoria }: { categoria: any }) {
+  const fotos = await obtenerFotos();
+  const fotosFiltradas = fotos.filter((foto: Foto) => {
+    return (
+      (categoria === "Politica" && foto.categoria === "Infografias") ||
+      (categoria === "Sociedad" && foto.categoria === "Fotografias")
+    );
+  });
+  const title = categoria === "Politica" ? "Infografias" : "Fotografias";
+
+  return (
+    <section className="col-start-2 col-end-7 flex flex-col bg-gradient-to-b from-pageColor to-gradientColor w-max m-auto p-8 pt-4 border border-pageColor rounded-3xl masRecientes_main_section text-white">
+      <h1 className="m-auto pb-2 text-3xl font-bold">{title}</h1>
+      <section className="flex">
+        {fotosFiltradas.map((foto: Foto, index: number) => (
+          <article
+            key={index}
+            className="px-4 m-auto flex flex-col items-center noticias_title"
+          >
+            <Image
+              className="rounded-xl"
+              src={modifyImageUrl(foto.photo.asset._ref)}
+              alt={foto.title}
+              height={400}
+              width={400}
+            />
+            <h1 className="text-2xl font-bold pt-2 fotos_title">
+              {foto.title}
+            </h1>
+          </article>
+        ))}
+      </section>
+    </section>
+  );
 }
