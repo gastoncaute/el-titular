cedf74fe12170b76099682a69c9e10905f2f2cb3
gastoncaute <gastoncaute@gmail.com>
2023-11-23 12:36:53 -0300

    dolar style

--- src/components/SectionComponents/components/ComponentesEntreSecciones/Dolar.tsx
+++ src/components/SectionComponents/components/ComponentesEntreSecciones/Dolar.tsx
@@ -6,21 +6,23 @@ export default async function Dolar() {
   const dolarHoy = await obtenerDolar();
   console.log(dolarHoy);
   return (
-    <section className="col-start-2 col-end-7 flex bg-gradient-to-b from-pageColor to-gradientColor w-max max-w-97.5rem m-auto p-4 border border-pageColor rounded-3xl fotos_widget_section text-white text-sm">
-      {dolarHoy.map((dolar: Dolar) => (
-        <section
-          key={dolar.casa.nombre}
-          className="flex flex-col w-max mx-2 p-2 border rounded-xl"
-        >
-          <h1 className="border-b mb-1">{dolar.casa.nombre}</h1>
-          <h2>Compra: $ {dolar.casa.compra}</h2>
-          {typeof dolar.casa.venta === "object" ? (
-            <p>Venta: No Disponible</p>
-          ) : (
-            <h3>Venta: $ {dolar.casa.venta}</h3>
-          )}
-        </section>
-      ))}
+    <section className="col-start-2 col-end-7 flex bg-gradient-to-b from-pageColor to-gradientColor w-max max-w-97.5rem m-auto p-4 border border-pageColor rounded-3xl dolar_widget_container text-white text-sm">
+      <div className="flex dolar_scroll">
+        {dolarHoy.map((dolar: Dolar) => (
+          <article
+            key={dolar.casa.nombre}
+            className="flex flex-col w-max mx-2 p-2 border rounded-xl dolar_article"
+          >
+            <h1 className="border-b mb-1">{dolar.casa.nombre}</h1>
+            <h2>Compra: $ {dolar.casa.compra}</h2>
+            {typeof dolar.casa.venta === "object" ? (
+              <p>Venta: No Disponible</p>
+            ) : (
+              <h3>Venta: $ {dolar.casa.venta}</h3>
+            )}
+          </article>
+        ))}
+      </div>
     </section>
   );
 }
