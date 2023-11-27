0207348111624b94ea5ed88a1af66cd6265a2336
gastoncaute <gastoncaute@gmail.com>
2023-11-27 18:26:53 -0300

    phone number

--- src/components/FooterComponents/Footer.tsx
+++ src/components/FooterComponents/Footer.tsx
@@ -69,7 +69,7 @@ export default function Footer() {
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
             </svg>
-            (223) 12345678
+            (223) 3480823
           </p>
         </h3>
         <h5 className="p-4 border-t w-full flex justify-center items-center">
