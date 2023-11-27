545d0ab05d5fc68109565c19f86dc90f09f97409
gastoncaute <gastoncaute@gmail.com>
2023-11-27 18:46:10 -0300

    bordes clima

--- src/components/SectionComponents/components/Clima.tsx
+++ src/components/SectionComponents/components/Clima.tsx
@@ -1,7 +1,7 @@
 export default function Clima() {
   return (
     <div className="col-start-1 col-end-2 row-start-1 flex justify-center clima_widget">
-      <div className="border border-pageColor m-8 fixed w-1/6 h-5/6 rounded-3xl flex justify-center items-center -mt-8 mr-4">
+      <div className="m-8 fixed w-1/6 h-5/6 flex justify-center items-center -mt-8 mr-4">
         <script
           src="https://static.elfsight.com/platform/platform.js"
           data-use-service-core
