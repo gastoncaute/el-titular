361df1fea0358c4197267a8674f3fba9497dad4d
gastoncaute <gastoncaute@gmail.com>
2023-11-28 20:00:44 -0300

    redes infografias

--- src/components/pagesComponents/FotosPage/Autor.tsx
+++ src/components/pagesComponents/FotosPage/Autor.tsx
@@ -10,9 +10,9 @@ export default function Autor({ categoria }: { categoria: any }) {
   const autorRedes =
     categoria === "Infografias"
       ? [
-          "https://instagram.com/rorolopez02/",
-          "https://instagram.com/rorolopez02/",
-          "https://instagram.com/rorolopez02/",
+          "https://www.instagram.com/juanaamonti/",
+          "https://www.montijuana1@gmail.com",
+          "https://wa.me/5491164522101",
         ]
       : [
           "https://instagram.com/gastoncauteruccio/",
@@ -66,7 +66,8 @@ export default function Autor({ categoria }: { categoria: any }) {
                 className="headersButtons social_buttons"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c0 -.249 1.51 -2.772 1.818 -4.013z" />
+                <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
+                <path d="M3 7l9 6l9 -6" />
               </svg>
             </Link>
           </li>
@@ -84,7 +85,8 @@ export default function Autor({ categoria }: { categoria: any }) {
                 className="headersButtons social_buttons"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M21 7.917v4.034a9.948 9.948 0 0 1 -5 -1.951v4.5a6.5 6.5 0 1 1 -8 -6.326v4.326a2.5 2.5 0 1 0 4 2v-11.5h4.083a6.005 6.005 0 0 0 4.917 4.917z" />
+                <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+                <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" />
               </svg>
             </Link>
           </li>
