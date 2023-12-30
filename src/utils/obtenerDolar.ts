7d0c63a772c5f81d3938c53fee87d2b2cc8ac28a
gastoncaute <gastoncaute@gmail.com>
2023-12-30 19:53:06 -0300

    Dolar API

--- src/utils/obtenerDolar.ts
+++ src/utils/obtenerDolar.ts
@@ -1,15 +1,15 @@
-// export async function obtenerDolar() {
-//   const res = await fetch(
-//     "https://www.dolarsi.com/api/api.php?type=valoresprincipales",
-//     {
-//       method: "GET",
-//       cache: "no-store",
-//     }
-//   );
-//   if (res.ok) {
-//     const data = await res.json();
-//     return data;
-//   } else {
-//     throw new Error("Error al obtener la API");
-//   }
-// }
+export async function obtenerDolar() {
+  const res = await fetch(
+    "https://www.dolarsi.com/api/api.php?type=valoresprincipales",
+    {
+      method: "GET",
+      cache: "no-store",
+    }
+  );
+  if (res.ok) {
+    const data = await res.json();
+    return data;
+  } else {
+    throw new Error("Error al obtener la API");
+  }
+}
