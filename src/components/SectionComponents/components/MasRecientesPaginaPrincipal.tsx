691be03a3ff30dbcb434ba0b3769d60266bddc7e
gastoncaute <gastoncaute@gmail.com>
2024-02-11 19:32:12 -0300

    title heigth

--- src/components/SectionComponents/components/MasRecientesPaginaPrincipal.tsx
+++ src/components/SectionComponents/components/MasRecientesPaginaPrincipal.tsx
@@ -42,7 +42,7 @@ export default async function MasRecientes() {
                     className={
                       index === 0
                         ? "h-32 text-5xl font-bold principal_page_title noticias_title mb-8"
-                        : "h-32 text-2xl font-bold principal_page_title principal_page_title_2 noticias_title mb-2"
+                        : "h-40 text-2xl font-bold principal_page_title principal_page_title_2 noticias_title mb-2"
                     }
                   >
                     {noticia.title}
