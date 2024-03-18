4e62d99405ccf8076201430dfdb177dba07752ba
gastoncaute <gastoncaute@gmail.com>
2024-03-18 20:10:28 -0300

    images array

--- src/components/pagesComponents/NoticePage/ArrayImages.tsx
+++ src/components/pagesComponents/NoticePage/ArrayImages.tsx
@@ -5,23 +5,25 @@ import React from "react";
 
 export default function ArrayImages({ arrayImages }: { arrayImages: any }) {
   return (
-    <section className="flex pt-8 bg-gradient-to-b from-pageColor to-gradientColor border border-black rounded-xl array_image_container">
-      <div className="array_image_scroll">
-        {arrayImages?.map((imagen: any) => (
-          <div key={imagen._key} className="m-auto array_image_article">
-            <Image
-              className="m-auto array_image_noticepage"
-              src={modifyImageUrl(imagen.imagen.asset._ref)}
-              alt={imagen.epigrafe[0]}
-              width={350}
-              height={250}
-            />
-            <h5 className="py-4 w-max m-auto array_image_noticepage_epigrafe">
-              <PortableText value={imagen.epigrafe} />
-            </h5>
-          </div>
-        ))}
-      </div>
+    <section>
+      {arrayImages?.map((imagen: any) => (
+        <div
+          key={imagen._key}
+          className="noticepage_div_image flex flex-col items-center"
+        >
+          <Image
+            className="m-auto noticepage_image"
+            src={modifyImageUrl(imagen.imagen.asset._ref)}
+            alt={imagen.epigrafe[0]}
+            height={800}
+            width={800}
+            style={{ maxHeight: "600px", maxWidth: "100%", width: "auto" }}
+          />
+          <h5 className="py-4 w-max m-auto array_image_noticepage_epigrafe">
+            <PortableText value={imagen.epigrafe} />
+          </h5>
+        </div>
+      ))}
     </section>
   );
 }
