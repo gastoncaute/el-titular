04af49252a84f65ba6a27951e52eb611caaf9445
gastoncaute <gastoncaute@gmail.com>
2024-04-08 23:57:28 -0300

    pop up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/HeaderComponents/Header";
 import Section from "@/components/SectionComponents/Section";
 import Footer from "@/components/FooterComponents/Footer";
-import PopUp from "@/components/SectionComponents/PopUp";
+// import PopUp from "@/components/SectionComponents/PopUp";
 
 export default function Home() {
   return (
