887b4b008757300d9ef7c13ebbad1ef625c541de
gastoncaute <gastoncaute@gmail.com>
2024-04-14 22:19:50 -0300

    input busqueda responsive

---
+++ src/components/Buttons/SeacrhInputButton.tsx
@@ -0,0 +1,58 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+
+export default function SeacrhInputButton() {
+  const [busqueda, setBusqueda] = useState("");
+  const handleChange = (e: any) => {
+    setBusqueda(e.target.value);
+  };
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Buscador"
+        className="px-2 mr-2 rounded-sm text-black"
+        value={busqueda}
+        onChange={handleChange}
+      />
+      {busqueda.trim() !== "" ? (
+        <Link href={`/pages/searchpage/${busqueda}`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-search"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#fff"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+            <path d="M21 21l-6 -6" />
+          </svg>
+        </Link>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-search"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="#fff"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+          <path d="M21 21l-6 -6" />
+        </svg>
+      )}
+    </>
+  );
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import CategoriesButton from "./Buttons/CategoriesButton";
 import SocialButtons from "./Buttons/SocialButtons";
+import SeacrhInputButton from "./Buttons/SeacrhInputButton";
 
 export default function NavBar() {
   return (
     <nav className="bg-gradient-to-b from-pageColor to-gradientColor w-96 h-screen flex flex-col justify-evenly absolute p-7 navbar">
       <ul className="flex flex-col h-full m-3 mt-8 navbar_ul">
+        <li className="py-4 input-busqueda-navbar w-min navbar_li">
+          <SeacrhInputButton />
+        </li>
         <li className="py-4 text-xl headersButtons w-min navbar_li">
           <Link href="/">Inicio</Link>
         </li>
