7e51df61add81308368178d6a65c9323721785cc
gastoncaute <gastoncaute@gmail.com>
2025-03-27 16:06:12 -0300

    Dolar style

--- src/components/SectionComponents/components/ComponentesEntreSecciones/Dolar.tsx
+++ src/components/SectionComponents/components/ComponentesEntreSecciones/Dolar.tsx
@@ -1,29 +1,21 @@
-import { Dolar } from "@/types/componentes.types";
+import type { Dolar } from "@/types/componentes.types";
 import { obtenerDolar } from "@/utils/obtenerDolar";
 import React from "react";
 
 export default async function Dolar() {
   const dolarHoy = await obtenerDolar();
   return (
-    <section className="col-start-2 col-end-7 flex bg-gradient-to-b from-pageColor to-gradientColor w-max max-w-97.5rem m-auto mb-8 p-4 border border-pageColor rounded-3xl dolar_widget_container text-white text-sm">
-      <div className="flex dolar_scroll">
+    <section className="dolar-section">
+      <h1>Cotizacion del Dolar</h1>
+      <div className="dolar_scroll">
         {dolarHoy.map((dolar: Dolar) => (
-          <article
-            key={dolar.nombre}
-            className="flex flex-col w-max mx-4 p-4 border rounded-xl dolar_article"
-          >
-            <h1 className="border-b mb-1 text-xl dolar_title">
-              {dolar.nombre}
-            </h1>
-            <h2 className="text-base dolar_subtitle">
-              Compra: $ {dolar.compra}
-            </h2>
+          <article key={dolar.nombre} className="dolar_article">
+            <h2>{dolar.nombre}</h2>
+            <h3>Compra: $ {dolar.compra}</h3>
             {typeof dolar.venta === "object" ? (
               <p>Venta: No Disponible</p>
             ) : (
-              <h3 className="text-base dolar_subtitle">
-                Venta: $ {dolar.venta}
-              </h3>
+              <h3>Venta: $ {dolar.venta}</h3>
             )}
           </article>
         ))}
