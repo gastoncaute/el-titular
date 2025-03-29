b7d3e75970c7b9a3f3e007839bae27395af4999b
gastoncaute <gastoncaute@gmail.com>
2025-03-29 08:46:19 -0300

    Category button error

--- src/components/Buttons/CategoriesButton.tsx
+++ src/components/Buttons/CategoriesButton.tsx
@@ -2,13 +2,21 @@ import Link from "next/link";
 
 export default function CategoriesButton() {
   const categorias = ["POLITICA", "ACTUALIDAD", "POLICIALES"];
+
   return (
     <>
-      {categorias.map((categoria: string) => (
-        <li className="button" key={categoria}>
-          <Link href={`/pages/categorypage/${categoria}`}>{categoria}</Link>
-        </li>
-      ))}
+      {categorias.map((categoria: string) => {
+        const categoriaFormateada =
+          categoria.charAt(0).toUpperCase() + categoria.slice(1).toLowerCase();
+
+        return (
+          <li className="button" key={categoria}>
+            <Link href={`/pages/categorypage/${categoriaFormateada}`}>
+              {categoria}
+            </Link>
+          </li>
+        );
+      })}
     </>
   );
 }
