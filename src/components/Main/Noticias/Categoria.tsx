2eac5bec18aee2ea3c01424638efccdbe0cfd925
gastoncaute <gastoncaute@gmail.com>
2025-04-03 10:46:51 -0300

    Categoria Background

--- src/components/Main/Noticias/Categoria.tsx
+++ src/components/Main/Noticias/Categoria.tsx
@@ -10,51 +10,117 @@ export default async function Categoria({ categoria }: { categoria: string }) {
   const noticiasDeCategoria = noticias.filter(
     (noticia: Noticia) => noticia.categoria === categoria
   );
+  const ultimaNoticia = noticiasDeCategoria[0];
 
   return (
     <>
-      {noticiasDeCategoria.slice(0, 4).map((noticia: Noticia) => (
-        <Link
-          href={`/pages/noticepage/${encodeURIComponent(noticia.title)}`}
-          key={noticia._id}
-        >
-          <h1>{noticia.title}</h1>
-          {noticia.image_principal && noticia.image_principal.imagen && (
-            <Image
-              src={modifyImageUrl(noticia.image_principal.imagen?.asset?._ref)}
-              alt={noticia.image_principal.epigrafe}
-              width={400}
-              height={150}
-              style={{
-                objectFit: "cover",
-                maxWidth: "100%",
-                maxHeight: "200px",
-              }}
-            />
-          )}
-          {(!noticia.image_principal || !noticia.image_principal.imagen) &&
-            noticia.image_principal?.video && (
-              <video
-                controls
-                width={400}
-                height={150}
-                style={{
-                  objectFit: "cover",
-                  maxWidth: "100%",
-                  maxHeight: "200px",
-                }}
-              >
-                <source
-                  src={modifyVideoFileUrl(
-                    noticia.image_principal.video.asset._ref
+      <section key={ultimaNoticia._id} className="noticia-section">
+        <div className="category-noticia-container">
+          <div className="category-text-overlay">
+            <Link
+              href={`/pages/categorypage/${categoria}`}
+              className="category-link-noticia"
+            >
+              {categoria}
+            </Link>
+            <h1>{ultimaNoticia.title}</h1>
+            <h2>{ultimaNoticia.bajada}</h2>
+            <Link
+              href={`/pages/noticepage/${ultimaNoticia.title}`}
+              className="category-leer-noticia"
+            >
+              LEER NOTICIA
+            </Link>
+          </div>
+          <div className="category-image-container">
+            {ultimaNoticia.image_principal &&
+              ultimaNoticia.image_principal.imagen && (
+                <Image
+                  src={modifyImageUrl(
+                    ultimaNoticia.image_principal.imagen?.asset?._ref
                   )}
-                  type="video/mp4"
+                  alt={ultimaNoticia.image_principal.epigrafe}
+                  width={700}
+                  height={600}
+                  style={{
+                    objectFit: "cover",
+                    maxWidth: "100%",
+                    maxHeight: "420px",
+                  }}
                 />
-                Your browser does not support the video tag.
-              </video>
-            )}
-        </Link>
-      ))}
+              )}
+            {(!ultimaNoticia.image_principal ||
+              !ultimaNoticia.image_principal.imagen) &&
+              ultimaNoticia.image_principal?.video && (
+                <video
+                  controls
+                  width={1500}
+                  height={600}
+                  style={{
+                    objectFit: "cover",
+                    maxWidth: "100%",
+                    maxHeight: "420px",
+                  }}
+                >
+                  <source
+                    src={modifyVideoFileUrl(
+                      ultimaNoticia.image_principal.video.asset._ref
+                    )}
+                    type="video/mp4"
+                  />
+                  Your browser does not support the video tag.
+                </video>
+              )}
+          </div>
+        </div>
+
+        <article>
+          {noticiasDeCategoria.slice(1, 5).map((noticia: Noticia) => (
+            <Link
+              href={`/pages/noticepage/${encodeURIComponent(noticia.title)}`}
+              key={noticia._id}
+            >
+              <h1>{noticia.title}</h1>
+              {noticia.image_principal && noticia.image_principal.imagen && (
+                <Image
+                  src={modifyImageUrl(
+                    noticia.image_principal.imagen?.asset?._ref
+                  )}
+                  alt={noticia.image_principal.epigrafe}
+                  width={400}
+                  height={150}
+                  style={{
+                    objectFit: "cover",
+                    maxWidth: "100%",
+                    maxHeight: "200px",
+                  }}
+                />
+              )}
+              {(!noticia.image_principal || !noticia.image_principal.imagen) &&
+                noticia.image_principal?.video && (
+                  <video
+                    controls
+                    width={400}
+                    height={150}
+                    style={{
+                      objectFit: "cover",
+                      maxWidth: "100%",
+                      maxHeight: "200px",
+                    }}
+                  >
+                    <source
+                      src={modifyVideoFileUrl(
+                        noticia.image_principal.video.asset._ref
+                      )}
+                      type="video/mp4"
+                    />
+                    Your browser does not support the video tag.
+                  </video>
+                )}
+            </Link>
+          ))}
+        </article>
+      </section>
     </>
   );
 }
--- src/components/Main/Noticias/Noticia.tsx
+++ src/components/Main/Noticias/Noticia.tsx
@@ -1,8 +1,6 @@
-import Link from "next/link";
 import { obtenerCategorias } from "@/utils/obtenerCategorias";
 import Categoria from "./Categoria";
 import Recientes from "./Recientes";
-import Image from "next/image";
 import Socials from "../Widgets/Socials";
 
 export default async function Noticia() {
@@ -16,36 +14,12 @@ export default async function Noticia() {
         ordenCategorias.indexOf(a) - ordenCategorias.indexOf(b)
     );
 
-  const imagenesPorCategoria: { [key: string]: string } = {
-    Politica: "/Politica.webp",
-    Actualidad: "/Actualidad.jpg",
-    Policiales: "/Policiales.avif",
-  };
-
   return (
     <>
       <Recientes />
       <Socials />
       {categoriasUnicas.map((categoria: any) => (
-        <section key={categoria} className="noticia-section">
-          <Link href={`/pages/categorypage/${categoria}`}>
-            <Image
-              src={imagenesPorCategoria[categoria]}
-              alt={categoria}
-              width={1500}
-              height={600}
-              style={{
-                objectFit: "cover",
-                maxWidth: "100%",
-                maxHeight: "600px",
-              }}
-            />
-          </Link>
-          <div className="banner-2"></div>
-          <article>
-            <Categoria categoria={categoria} />
-          </article>
-        </section>
+        <Categoria key={categoria} categoria={categoria} />
       ))}
     </>
   );
