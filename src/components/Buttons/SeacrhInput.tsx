f39e4fb512aeb04e093e72860a51dd2c75b1aed5
gastoncaute <gastoncaute@gmail.com>
2025-04-25 15:15:24 -0300

    Responsive Search Input

--- src/components/Buttons/SeacrhInput.tsx
+++ src/components/Buttons/SeacrhInput.tsx
@@ -4,8 +4,15 @@ import React, { useState } from "react";
 
 export default function SeacrhInput() {
   const [busqueda, setBusqueda] = useState("");
-  const handleChange = (e: any) => {
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setBusqueda(e.target.value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && busqueda.trim() !== "") {
+      window.location.href = `/pages/searchpage/${busqueda.trim()}`;
+    }
   };
   return (
     <>
@@ -16,33 +23,15 @@ export default function SeacrhInput() {
         value={busqueda}
         onChange={handleChange}
       />
-      {busqueda.trim() !== "" ? (
-        <Link href={`/pages/searchpage/${busqueda}`}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="icon icon-tabler icon-tabler-search"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="#fff"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-            <path d="M21 21l-6 -6" />
-          </svg>
-        </Link>
-      ) : (
+      <Link
+        href={busqueda.trim() !== "" ? `/pages/searchpage/${busqueda}` : "#"}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="icon icon-tabler icon-tabler-search"
-          width="16"
-          height="16"
+          width="30"
+          height="30"
           viewBox="0 0 24 24"
-          strokeWidth="1.5"
+          strokeWidth="1.2"
           stroke="#fff"
           fill="none"
           strokeLinecap="round"
@@ -52,7 +41,7 @@ export default function SeacrhInput() {
           <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
           <path d="M21 21l-6 -6" />
         </svg>
-      )}
+      </Link>
     </>
   );
 }
