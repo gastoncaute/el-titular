b9e9d5a04eb8d064688c685d137e2ea4b087d76b
gastoncaute <gastoncaute@gmail.com>
2026-09-21 21:45:46 -0300

    input style

--- src/components/Header/InputBusqueda.tsx
+++ src/components/Header/InputBusqueda.tsx
@@ -23,8 +23,8 @@ export default function InputBusqueda() {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="search-icon"
-          width="32"
-          height="32"
+          width="24"
+          height="24"
           viewBox="0 0 24 24"
           strokeWidth="1.5"
           stroke="#111318"
